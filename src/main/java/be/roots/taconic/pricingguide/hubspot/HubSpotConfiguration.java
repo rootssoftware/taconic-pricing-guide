0d9566b29f393c66cca7b9c42be2a983f13d9b34
Koen Dehaen <koen.dehaen@roots.be>
2023-04-13 11:36:08 +0200

    4.22 update on used dependencies

---
+++ src/main/java/be/roots/taconic/pricingguide/hubspot/HubSpotConfiguration.java
@@ -0,0 +1,47 @@
+package be.roots.taconic.pricingguide.hubspot;
+
+/*
+   This file is part of the Taconic Pricing Guide generator.  This code will
+   generate a full featured PDF Pricing Guide by using using iText
+   (http://www.itextpdf.com) based on JSON files.
+
+   Copyright (C) 2015  Roots nv
+   Authors: Koen Dehaen (koen.dehaen@roots.be)
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For more information, please contact Roots nv at this address: support@roots.be
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HubSpotConfiguration {
+
+    @Value("${api.token}")
+    private String apiToken;
+
+    @Bean
+    public RestTemplate hubSpotRestTemplate() {
+        return new RestTemplateBuilder()
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiToken)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .build();
+    }
+}
